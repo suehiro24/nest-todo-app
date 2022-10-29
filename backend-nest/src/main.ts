d62aa242e4ceac306baa8f05be67f5e016623ad9
Suehiro24 <hayatasato.0@gmail.com>
2022-10-29 10:13:51 +0900

    feat: load .env

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.BACKEND_NEST_CONTAINER_PORT || '3000'));
 }
 bootstrap();
