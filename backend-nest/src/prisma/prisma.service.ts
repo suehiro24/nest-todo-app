ca216558476de1e995dbc3d018034a1b633b686c
Suehiro24 <hayatasato.0@gmail.com>
2022-10-30 22:36:49 +0900

    feat(prisma): add PrismaModule and PrismaService (Prisma Client wrapper)

---
+++ backend-nest/src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      // see: https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
+      await app.close();
+    });
+  }
+}
