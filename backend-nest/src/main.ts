613638c698b7fd8bcaf72e7107ccc4e5cab40bcf
Suehiro24 <hayatasato.0@gmail.com>
2022-10-30 23:22:01 +0900

    feat(prisma): make closeable nest application explicitly before Prisma Client calls exit.process()

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { PrismaService } from './prisma/prisma.service';
 
 dotenv.config({ path: '.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(parseInt(process.env.BACKEND_NEST_CONTAINER_PORT || '3000'));
 }
 bootstrap();
