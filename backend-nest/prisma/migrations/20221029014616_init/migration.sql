6d236554283fe812e87afd3f642f1b31549b1700
Suehiro24 <hayatasato.0@gmail.com>
2022-10-30 22:24:13 +0900

    run prisma migrate

--- backend-nest/prisma/migrations/20221029014616_init/migration.sql
+++ backend-nest/prisma/migrations/20221029014616_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
-    "passowrd" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
